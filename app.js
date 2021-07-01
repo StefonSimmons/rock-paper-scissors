89adf38a0c82f8200357128cb7437b5ec86def99
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-06-30 21:07:28 -0400

    showing my and house choices

---
+++ app.js
@@ -0,0 +1,38 @@
+const choices = document.querySelectorAll('.choice')
+const choicesSection = document.querySelector(".choices-section")
+const chosenSection = document.querySelector(".chosen-section")
+const myChosenContent = document.querySelector(".my-chosen-content")
+const houseContent = document.querySelector(".house-chosen-content")
+
+const selectionTemplate = (choice) => {
+  const selection = `
+  <div class="${choice}-choice-color choice">
+    <div class="inner">
+      <img
+        src="./images/icon-${choice}.svg"
+        alt="${choice}"
+        class="${choice}-btn"
+      />
+    </div>
+  </div>
+`
+  return selection
+}
+
+const getChoice = (choiceNode) => {
+  const myChoice = choiceNode.value
+  const houseChoice = choices[Math.floor(Math.random() * choices.length)].value
+  
+  myChosenContent.insertAdjacentHTML('beforeend', selectionTemplate(myChoice));
+  houseContent.insertAdjacentHTML('beforeend', selectionTemplate(houseChoice));
+
+}
+
+choices.forEach(choice => {
+  choice.addEventListener('click', () => {
+    choicesSection.style.display = 'none'
+    chosenSection.style.display = 'flex'
+    getChoice(choice)
+  })
+})
+
