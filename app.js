b5ed6c2979b79d2b55827e034ebe9276c8c2983e
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-06-30 21:53:39 -0400

    game logic set

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const choicesSection = document.querySelector(".choices-section")
 const chosenSection = document.querySelector(".chosen-section")
 const myChosenContent = document.querySelector(".my-chosen-content")
 const houseContent = document.querySelector(".house-chosen-content")
+const score = document.querySelector(".score")
 
 const selectionTemplate = (choice) => {
   const selection = `
@@ -11,7 +12,6 @@ const selectionTemplate = (choice) => {
       <img
         src="./images/icon-${choice}.svg"
         alt="${choice}"
-        class="${choice}-btn"
       />
     </div>
   </div>
@@ -19,20 +19,38 @@ const selectionTemplate = (choice) => {
   return selection
 }
 
-const getChoice = (choiceNode) => {
+const appendChoices = (choiceNode) => {
   const myChoice = choiceNode.value
   const houseChoice = choices[Math.floor(Math.random() * choices.length)].value
   
   myChosenContent.insertAdjacentHTML('beforeend', selectionTemplate(myChoice));
   houseContent.insertAdjacentHTML('beforeend', selectionTemplate(houseChoice));
+  return [myChoice, houseChoice]
+}
 
+const checkPicks = (picks) => {
+  const [myChoice, houseChoice] = picks
+  if (myChoice === "paper" && houseChoice === "rock") {
+    score.textContent = parseInt(score.textContent) + 1
+  } else if (myChoice === "rock" && houseChoice === "paper") {
+    score.textContent = parseInt(score.textContent) - 1
+  } else if (myChoice === "scissors" && houseChoice === "paper") {
+    score.textContent = parseInt(score.textContent) + 1
+  } else if (myChoice === "paper" && houseChoice === "scissors") {
+    score.textContent = parseInt(score.textContent) - 1
+  } else if (myChoice === "rock" && houseChoice === "scissors") {
+    score.textContent = parseInt(score.textContent) + 1
+  } else if (myChoice === "scissors" && houseChoice === "rock") {
+    score.textContent = parseInt(score.textContent) - 1
+  }
 }
 
 choices.forEach(choice => {
   choice.addEventListener('click', () => {
     choicesSection.style.display = 'none'
     chosenSection.style.display = 'flex'
-    getChoice(choice)
+    const selections = appendChoices(choice)
+    checkPicks(selections)
   })
 })
 
